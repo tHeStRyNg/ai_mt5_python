8e4bd87d03e6e2b50fd82c405a6b8d03dfa1a3eb
tHeStRyNg <info@algorithmic.one>
2023-02-20 12:09:37 +0100

    refactored TEST_ORDER

--- bot/test_order.py
+++ bot/test_order.py
@@ -1,15 +1,13 @@
 import MetaTrader5 as mt5
  
-# mostramos los datos sobre el paquete MetaTrader5
+
 print("MetaTrader5 package author: ", mt5.__author__)
 print("MetaTrader5 package version: ", mt5.__version__)
  
-# establecemos la conexión con el terminal MetaTrader 5
 if not mt5.initialize():
     print("initialize() failed, error code =",mt5.last_error())
     quit()
  
-# preparamos la estructura de la solicitud de compra
 symbol = "EURUSD"
 symbol_info = mt5.symbol_info(symbol)
 if symbol_info is None:
@@ -17,7 +15,6 @@ if symbol_info is None:
     mt5.shutdown()
     quit()
  
-# si el símbolo no está disponible en MarketWatch, lo añadimos
 if not symbol_info.visible:
     print(symbol, "is not visible, trying to switch on")
     if not mt5.symbol_select(symbol,True):
@@ -44,17 +41,13 @@ request = {
     "type_filling": mt5.ORDER_FILLING_FOK,
 }
  
-# enviamos la solicitud comercial
 result = mt5.order_send(request)
-# comprobamos el resultado de la ejecución
 print("1. order_send(): by {} {} lots at {} with deviation={} points".format(symbol,lot,price,deviation));
 if result.retcode != mt5.TRADE_RETCODE_DONE:
     print("2. order_send failed, retcode={}".format(result.retcode))
-   # solicitamos el resultado en forma de diccionario y lo mostramos elemento por elemento
     result_dict=result._asdict()
     for field in result_dict.keys():
         print("   {}={}".format(field,result_dict[field]))
-        # si se trata de la estructura de una solicitud comercial, también la mostramos elemento por elemento
         if field=="request":
             traderequest_dict=result_dict[field]._asdict()
             for tradereq_filed in traderequest_dict:
